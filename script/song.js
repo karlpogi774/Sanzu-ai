e4ff2cf171bc2abfbb86e9ae6478b1bbc0d215d0
karlpogi774 <Karlpogi774@gmail.com>
2026-09-18 09:53:09 +0800

    Add song request module configuration
    Signed-off-by: karlpogi774 <Karlpogi774@gmail.com>

---
+++ script/song.js
@@ -0,0 +1,11 @@
+module.exports.config = {
+  name: "songrequest",
+  version: "1.0.0",
+  hasPermission: 0,
+  credits: "Gojo",
+  description: "Gumawa ng song request message",
+  commandCategory: "Fun",
+  usages: "songrequest <song title> - <artist>",
+  usePrefix: true,
+  cooldowns: 3
+};
